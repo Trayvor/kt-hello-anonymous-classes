e2d3858260efe0828db1ad167078bbd9bce2f990
Vlad <vladyslav.herasymenkoo@gmail.com>
2024-11-20 16:21:21 +0100

    done

--- src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
+++ src/main/kotlin/mate/academy/HelloWorldAnonymousClasses.kt
@@ -8,6 +8,33 @@ class HelloWorldAnonymousClasses {
     }
 
     fun sayHello(names: List<String>): List<String> {
-        // TODO: implement
+        val list = mutableListOf<String>()
+
+        val englishGreeting = object : HelloWorldGreeting {
+            override fun greet() = "Hello world"
+            override fun greetSomeone(someone: String) = "Hello, $someone"
+        }
+
+        val frenchGreeting = object: HelloWorldGreeting {
+            override fun greet() = "Bonjour tout le monde"
+            override fun greetSomeone(someone: String) = "Bonjour, $someone"
+        }
+
+        val spanishGreeting = object: HelloWorldGreeting{
+            override fun greet() = "Hola mundo"
+            override fun greetSomeone(someone: String) = "Hola, $someone"
+        }
+
+        list.add(englishGreeting.greet())
+        list.add(frenchGreeting.greet())
+        list.add(spanishGreeting.greet())
+
+        for (name in names) {
+            list.add(englishGreeting.greetSomeone(name))
+            list.add(frenchGreeting.greetSomeone(name))
+            list.add(spanishGreeting.greetSomeone(name))
+        }
+
+        return list
     }
 }
